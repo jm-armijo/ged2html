e9025b4ca0215d2850f8fca029bc65d1db76f3ac
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2019-08-29 18:14:52 +1000

    Added Unit Tests for class Edge

---
+++ src/tests/edge.py
@@ -0,0 +1,41 @@
+import unittest
+from unittest.mock import Mock, MagicMock
+from ..edge import Edge
+
+class TestEdge(unittest.TestCase):
+
+	##########################################
+	# Edge.__init__
+	##########################################
+
+	def test_init_001(self):
+		start = Mock()
+		end = Mock()
+		edge = Edge(start, end)
+		self.assertEqual(edge.start, start)
+		self.assertEqual(edge.end, end)
+
+	##########################################
+	# Edge.toHTML
+	##########################################
+
+	def test_init_001(self):
+		start = 'X'
+		end = 'Y'
+
+		expected = (
+			'        new LeaderLine(\n'
+			'          document.getElementById("X"),\n'
+			'          document.getElementById("Y"),\n'
+			'        );\n'
+		)
+
+		edge = Edge.__new__(Edge)
+		edge.start = start
+		edge.end = end
+
+		actual = edge.toHTML()
+		self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+	unittest.main()
