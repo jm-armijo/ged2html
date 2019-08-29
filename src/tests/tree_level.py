93fd35fd3d538242e27fa54430ab5f32d9a47759
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2019-08-29 17:35:02 +1000

    Adding Unit Tests for class TreeLevel

---
+++ src/tests/tree_level.py
@@ -0,0 +1,68 @@
+import unittest
+from unittest.mock import MagicMock, Mock, patch, ANY
+from ..html import HTMLGenerator
+from ..tree_level import TreeLevel
+
+class TestTreeLevel(unittest.TestCase):
+
+	##########################################
+	# TreeLevel.init
+	##########################################
+
+	def test_init_001(self):
+		tree_level = TreeLevel()
+		self.assertEqual(tree_level.nodes, list())
+
+	##########################################
+	# TreeLevel.append
+	##########################################
+
+	@patch("src.tree_node.TreeNode.__new__")
+	def test_append_001(self, class_treenode):
+		node = Mock()
+		tree_node = Mock()
+		class_treenode.return_value = tree_node
+
+		tree_level = TreeLevel.__new__(TreeLevel)
+		tree_level.nodes = list()
+
+		tree_level.append(node)
+		class_treenode.assert_called_once_with(ANY, node)
+		self.assertEqual(tree_level.nodes, [tree_node])
+
+	@patch("src.tree_node.TreeNode.__new__")
+	def test_append_002(self, class_treenode):
+		node = Mock()
+		tree_node1 = Mock()
+		tree_node2 = Mock()
+		class_treenode.return_value = tree_node2
+
+		tree_level = TreeLevel.__new__(TreeLevel)
+		tree_level.nodes = [tree_node1]
+
+		tree_level.append(node)
+		class_treenode.assert_called_once_with(ANY, node)
+		self.assertEqual(tree_level.nodes, [tree_node1, tree_node2])
+
+	##########################################
+	# TreeLevel.toHTML
+	##########################################
+
+	def test_to_html_001(self):
+		html_list = "<div><div>X</div><div>Y</div></div>"
+		html_level = "<div>"+html_list+"</div>"
+
+		HTMLGenerator.listToHTML = MagicMock(return_value = html_list)
+		HTMLGenerator.wrap = MagicMock(return_value = html_level)
+
+		nodes = Mock()
+		tree_level = TreeLevel.__new__(TreeLevel)
+		tree_level.nodes = nodes
+
+		return_value = tree_level.toHTML()
+		HTMLGenerator.listToHTML.assert_called_once_with(nodes)
+		HTMLGenerator.wrap.assert_called_once_with(tree_level, html_list)
+		self.assertEqual(return_value, html_level)
+
+if __name__ == '__main__':
+	unittest.main()
