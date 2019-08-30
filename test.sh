082d875a0c73d74c2ae60df7318e1c6973f71297
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2019-08-30 16:33:51 +1000

    Adding script to run Unit Tests Adding Sample ged file (Japanese Imperial Family) Updaging .gitignore file

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+printf "\n\nRunning Line tests...\n"
+python3 -m unittest src.tests.text_line
+
+printf "\n\nRunning Node tests...\n"
+python3 -m unittest src.tests.node
+
+printf "\n\nRunning Union tests...\n"
+python3 -m unittest src.tests.union
+
+printf "\n\nRunning Person tests...\n"
+python3 -m unittest src.tests.person
+
+printf "\n\nRunning NUllPerson tests...\n"
+python3 -m unittest src.tests.null_person
+
+printf "\n\nRunning Parser tests...\n"
+python3 -m unittest src.tests.parser
+
+printf "\n\nRunning Tree tests...\n"
+python3 -m unittest src.tests.tree
+
+printf "\n\nRunning TreeLevel tests...\n"
+python3 -m unittest src.tests.tree_level
+
+printf "\n\nRunning TreeNode tests...\n"
+python3 -m unittest src.tests.tree_node
+
+printf "\n\nRunning unique_queue tests...\n"
+python3 -m unittest src.tests.unique_queue
+
+printf "\n\nRunning Edge tests...\n"
+python3 -m unittest src.tests.edge
+
+printf "\n\nRunning HTMLGenerator tests...\n"
+python3 -m unittest src.tests.html
