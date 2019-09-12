c377147b755b697ca4524e35382f4ce198121502
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2019-09-12 22:01:44 +1000

    Passing inout ged file by argument to the script. Split the main script into functions

--- run.py
+++ run.py
@@ -4,23 +4,39 @@ for a given .ged file.
 """
 
 import os
+import sys
 from shutil import copyfile
 from src.html import HTMLGenerator
 from src.parser import Parser
 
-# Make directories
-os.makedirs('html/css', 755, True)
-os.makedirs('html/scripts', 755, True)
+def generate_html_document(file_name):
+    parser = Parser()
+    tree = parser.make_tree(file_name)
+    html_tree = tree.to_html()
 
-# Copy files
-copyfile('src/css/styles.css', 'html/css/styles.css')
-copyfile('src/scripts/leader-line.min.js', 'html/scripts/leader-line.min.js')
+    title = "My Genealogy Tree"
+    doc = HTMLGenerator('html/index.html')
+    doc.generate(title, html_tree)
 
-file_name = '../tree.ged'
-parser = Parser()
-tree = parser.make_tree(file_name)
-html_tree = tree.to_html()
+def create_output_directory():
+    # Make directories
+    os.makedirs('html/css', 755, True)
+    os.makedirs('html/scripts', 755, True)
 
-title = "My Genealogy Tree"
-doc = HTMLGenerator('html/index.html')
-doc.generate(title, html_tree)
+    # Copy files
+    copyfile('src/css/styles.css', 'html/css/styles.css')
+    copyfile('src/scripts/leader-line.min.js', 'html/scripts/leader-line.min.js')
+
+def are_args_valid():
+    num_args = len(sys.argv)
+    if num_args != 2:
+        print("Expected 1 argument, received {}".format(num_args -1))
+        return False
+    else:
+        return True
+
+if are_args_valid():
+    file_name = sys.argv[1]
+    create_output_directory()
+    generate_html_document(file_name)
+
