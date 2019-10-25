cb868c5d2e0de5cedbc367345761d93dfb2961ed
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2019-10-25 21:10:44 +1100

    Displaying sources on each profile page

--- src/source.py
+++ src/source.py
@@ -19,23 +19,37 @@ class Source():
         return str(element)
 
     def _source_title_to_html(self):
+        title = HTMLElement('div')
+        title.add_attribute('class', 'table-title')
+        title.set_value(self.id)
+
         element = HTMLElement('div')
-        element.add_attribute('class', 'source-title')
-        element.set_value(self.id)
+        element.add_attribute('class', 'table-header')
+        element.set_value(str(title))
 
         return str(element)
 
     def _source_content_to_html(self):
         html = ''
         for object in self.objects:
-            content = HTMLElement('img')
-            content.add_attribute('class', 'source')
-            content.add_attribute('src', "../{}".format(object.file))
-
-            html += str(content)
+            html += self._object_to_html(object)
 
         element = HTMLElement('div')
         element.add_attribute('class', 'source-content')
         element.set_value(html)
 
         return str(element)
+
+    def _object_to_html(self, object):
+        path_to_image = "../{}".format(object.file)
+
+        content = HTMLElement('img')
+        content.add_attribute('class', 'source')
+        content.add_attribute('src', path_to_image)
+
+        link = HTMLElement('a')
+        link.add_attribute('href', path_to_image)
+        link.add_attribute('target', '_blank')
+        link.set_value(str(content))
+
+        return str(link)
